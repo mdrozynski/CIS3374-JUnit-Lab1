2aa3323b6bd883b890fd6d4db6e8a596591c622f
tud03372 <tud03372@CIS-SE20428.tu.temple.edu>
2015-02-06 10:05:59 -0500

    Finished

---
+++ Lab1/Test/com/lab1/calculator/CalculatorTestTest.java
@@ -0,0 +1,47 @@
+package com.lab1.calculator;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTestTest {
+	
+	private Calculator calculator;
+	
+	@Before
+	public void setUp() {
+		calculator = new Calculator();
+	}
+
+	@Test
+	public void addTest() {
+		assertEquals(4, calculator.add(2, 2));
+
+		assertTrue(calculator.isEqual(2, 2));
+		
+	}
+	@Test
+	public void subtractionTest() {
+		assertEquals(2, calculator.subtract(4,2));
+		
+	}
+	@Test
+	public void multiplicationTest() {
+		assertEquals(12, calculator.multiply(3, 4));
+		}
+	@Test
+	public void divisionTest() {
+				assertEquals(4, calculator.divide(8, 2));
+
+	}
+	@Test
+	public void isEqualTest() {
+				assertTrue(calculator.isEqual(2, 2));
+		
+	}
+	
+
+}
